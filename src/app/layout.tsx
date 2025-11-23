6197255276c42f0ff2df708b793899904df45f3c
speudoname <levan@sarke.ge>
2025-11-23 09:47:18 +0400

    Add favicon and site manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,17 @@ const inter = Inter({ subsets: ['latin', 'latin-ext'] })
 export const metadata: Metadata = {
   title: 'Nebiswera',
   description: 'Welcome to nebiswera.com',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({

f5f4bc1b5274f50b2c621d3793ab0d4d39dc9f13
speudoname <levan@sarke.ge>
2025-11-23 09:53:24 +0400

    Exclude static files from middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -115,5 +115,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.png|.*\\.ico|.*\\.webmanifest).*)'],
 }
